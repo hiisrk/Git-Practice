d95675f083c854a86a576f2021f86c19f0dfa493
monesh <monesh.garikina82@gmail.com>
2024-09-07 16:57:17 +0530

    modified student pojo

--- src.main.java/src/main/java/org/example/models/Student.java
+++ src.main.java/src/main/java/org/example/models/Student.java
@@ -12,4 +12,19 @@ public class Student {
     public void setName(String name) {
         this.name = name;
     }
+
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Student(String name, Integer id) {
+        this.name = name;
+        this.id = id;
+    }
 }
